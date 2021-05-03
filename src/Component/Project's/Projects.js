49f1bd9a7402c5e59a3012f665d8572910026dc1
Fatiha Zannat <fatihazannat.n@gmail.com>
2021-05-04 02:14:35 +0600

    allah

--- src/Component/Project's/Projects.js
+++ src/Component/Project's/Projects.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import img from '../../image/creative-agency.PNG'
-import img1 from '../../image/drive.PNG'
-import img2 from '../../image/league.PNG'
+import pic from '../../image/creative-agency.PNG'
+import pic1 from '../../image/drive.PNG'
+import pic2 from '../../image/league.PNG'
 
 
 const Projects = () => {
@@ -10,7 +10,7 @@ const Projects = () => {
         <div className="mt-4">
              <div class="card-group">
   <div class="card m-3">
-    <img class="card-img-top" src={img} alt="Card image cap"/>
+    <img class="card-pic-top" src={pic} alt="Card image cap"/>
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -20,7 +20,7 @@ const Projects = () => {
     </div>
   </div>
   <div class="card m-3">
-    <img class="card-img-top" src={img1} alt="Card image cap"/>
+    <img class="card-pic-top" src={pic1} alt="Card image cap"/>
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -30,7 +30,7 @@ const Projects = () => {
     </div>
   </div>
   <div class="card m-3">
-    <img class="card-img-top" src={img2} alt="Card image cap"/>
+    <img class="card-pic-top" src={pic2} alt="Card image cap"/>
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
